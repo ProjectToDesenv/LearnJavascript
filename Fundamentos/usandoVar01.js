5e5c2ab764e05b4968a122eca6874784b7d00ccd
Marcelo Matos dos Santos <marcelo.santos@sanesul.ms.gov.br>
2018-05-21 16:22:14 -0400

    Aulas 21/05/2018

---
+++ Fundamentos/usandoVar01.js
@@ -0,0 +1,16 @@
+{{{{var sera = 'Será ???'}}}}
+
+console.log(sera)
+
+function teste(){
+
+    var local = 123
+    let local2 = 1234
+}
+
+teste()
+
+//console.log(local)
+console.log(local2)
+
+
---
+++ Fundamentos/usandoVar02.js
@@ -0,0 +1,9 @@
+var numero = 1
+{
+    var numero = 2
+    console.log('Esta Dentro:', numero)
+}
+
+console.log('Está Fora:', numero )
+
+// Fuja do escopo Global
