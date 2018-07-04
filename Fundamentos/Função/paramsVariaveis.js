c824fabeae5530e3b1c6ab9265dd2641f3ca4db5
Marcelo Matos dos Santos <marcelo.santos@sanesul.ms.gov.br>
2018-07-04 17:26:34 -0400

    Aula 04-07-2018

---
+++ Fundamentos/Função/paramsVariaveis.js
@@ -0,0 +1,14 @@
+function soma(){
+    let soma = 0
+    // arguments é um array
+    for (i in arguments ){
+
+        soma += arguments[i]
+    }
+
+    return soma
+}
+
+console.log(soma())
+console.log(soma(1))
+console.log(soma(1.1,2.2,"Teste"))
